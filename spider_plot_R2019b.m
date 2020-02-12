24f818a7ac9bf19afd982faf97ec702d5611cc9b
NewGuy012 <juyoung.m.yoo@gmail.com>
2020-02-12 10:36:32 -0800

    Update spider_plot_R2019b.m

--- spider_plot_R2019b.m
+++ spider_plot_R2019b.m
@@ -197,6 +197,8 @@ function spider_plot_R2019b(P, options)
 % 
 % Author:
 %   Moses Yoo, (jyoo at hatci dot com)
+%	2020-02-12: Fixed condition and added error checking for when only one
+%			    data group is plotted.
 %   2020-01-27: Corrected bug where only 7 entries were allowed in legend.
 %   2020-01-06: Added support for tiledlayout feature introduced in R2019b.
 %   2019-11-27: Add option to change axes to logarithmic scale.
@@ -215,8 +217,8 @@ function spider_plot_R2019b(P, options)
 %
 % Special Thanks:
 %   Special thanks to Gabriela Andrade, Andrés Garcia, Jiro Doke,
-%   Alex Grenyer, Omar Hadri & Zafar Ali for their feature recommendations
-%   and suggested bug fixes.
+%   Alex Grenyer, Omar Hadri, Zafar Ali & Christophe Hurlin for
+%   their feature recommendations and suggested bug fixes.
 
 %%% Argument Validation %%%
 arguments
