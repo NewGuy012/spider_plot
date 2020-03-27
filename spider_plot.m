39194e6725a89f0a5e5a8d4071e3661dc02e48df
NewGuy012 <juyoung.m.yoo@gmail.com>
2020-03-27 12:39:49 -0700

    Update spider_plot.m

--- spider_plot.m
+++ spider_plot.m
@@ -191,8 +191,8 @@ function spider_plot(P, varargin)
 %   Moses Yoo, (jyoo at hatci dot com)
 %   2020-03-26: Added feature to allow different line styles, line width,
 %               marker type, and marker sizes for the data groups.
-%	2020-02-12: Fixed condition and added error checking for when only one
-%			    data group is plotted.
+%   2020-02-12: Fixed condition and added error checking for when only one
+%               data group is plotted.
 %   2020-01-27: Corrected bug where only 7 entries were allowed in legend.
 %   2020-01-06: Added support for subplot feature.
 %   2019-11-27: Add option to change axes to logarithmic scale.
